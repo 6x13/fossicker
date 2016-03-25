9a113cbdde21134459544f5ed6ab28b45b067f65
Kenan Bölükbaşı <kenanbolukbasi@gmail.com>
2016-03-25 06:50:43 +0200

    Added copyright to interaction source.

--- core/interaction.lisp
+++ core/interaction.lisp
@@ -1,4 +1,23 @@
-;;;; interaction.lisp
+;;;; -*- Mode: LISP; Syntax: COMMON-LISP; Package: INTERACTION -*-
+;; Copyright (C) 2015 Kenan Bölükbaşı
+
+;; Author: Kenan Bölükbaşı <kenanbolukbasi@gmail.com>
+;; Created: 10 March 2016
+
+;; This file is part of Fossicker.
+
+;; Fossicker is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; Fossicker is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with Fossicker.  If not, see <http://www.gnu.org/licenses/>.
 
 (defpackage #:interaction
   (:use #:cl)
@@ -6,6 +25,13 @@
    #:compile-interaction))
 
 (in-package #:interaction)
+
+;;;;;;;;;;;;;;;;
+;;; Interactions
+;;
+;;
+;;;; Notes
+;;
 
 #|
 handle t and nil interactions
