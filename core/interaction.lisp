24ae78264f0a9a66e83afceb053e94914d057882
Kenan Bölükbaşı <kenanbolukbasi@gmail.com>
2017-09-17 10:02:33 +0000

    Revised licence strings.

--- core/interaction.lisp
+++ core/interaction.lisp
@@ -6,18 +6,18 @@
 
 ;; This file is part of Fossicker.
 
-;; Fossicker is free software: you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
+;; Fossicker is free  software: you can redistribute it and/or  modify it under
+;; the  terms of  the  GNU General  Public  License as  published  by the  Free
+;; Software Foundation,  either version 3 of  the License, or (at  your option)
+;; any later version.
 
-;; Fossicker is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
+;; Fossicker is distributed in the hope that it will be useful, but WITHOUT ANY
+;; WARRANTY; without  even the implied  warranty of MERCHANTABILITY  or FITNESS
+;; FOR  A PARTICULAR  PURPOSE.  See  the GNU  General Public  License for  more
+;; details.
 
-;; You should have received a copy of the GNU General Public License
-;; along with Fossicker.  If not, see <http://www.gnu.org/licenses/>.
+;; You should have received a copy of the GNU General Public License along with
+;; Fossicker.  If not, see <http://www.gnu.org/licenses/>.
 
 (defpackage #:interaction
   (:use #:cl)
--- fossicker.asd
+++ fossicker.asd
@@ -1,4 +1,23 @@
 ;;;; -*- Mode: LISP; Syntax: COMMON-LISP; Package: ASDF-USER -*-
+;; Copyright (C) 2015 Kenan Bölükbaşı
+
+;; Author: Kenan Bölükbaşı <kenanbolukbasi@gmail.com>
+;; Created: 23 October 2015
+
+;; This file is part of Fossicker.
+
+;; Fossicker is free  software: you can redistribute it and/or  modify it under
+;; the  terms of  the  GNU General  Public  License as  published  by the  Free
+;; Software Foundation,  either version 3 of  the License, or (at  your option)
+;; any later version.
+
+;; Fossicker is distributed in the hope that it will be useful, but WITHOUT ANY
+;; WARRANTY; without  even the implied  warranty of MERCHANTABILITY  or FITNESS
+;; FOR  A PARTICULAR  PURPOSE.  See  the GNU  General Public  License for  more
+;; details.
+
+;; You should have received a copy of the GNU General Public License along with
+;; Fossicker.  If not, see <http://www.gnu.org/licenses/>.
 
 #-asdf3.1 (error "Fossicker requires ASDF 3.1")
 (defsystem #:fossicker
